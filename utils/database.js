0afdab63f2e42c15611c50db53620e59a1f35ac9
Hennadii Borysevych <genuch3@gmail.com>
2023-05-23 23:13:27 +0300

    feat:structure of project

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_promt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+    console.log("is Connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
